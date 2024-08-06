d67c3de6020aea7ea41cf7c709194b14d480cc4e
chenHX <1575197604@qq.com>
2024-08-06 09:25:59 +0800

    add coverage

--- total_python/t1/original.py
+++ total_python/t1/original.py
@@ -2,6 +2,14 @@ from typing import Union
 
 
 def smart_convert(value: str) -> Union[int, float, str]:
+    """_summary_
+
+    Args:
+        value (str): _description_
+
+    Returns:
+        Union[int, float, str]: _description_
+    """
     try:
         return int(value)
     except ValueError:
