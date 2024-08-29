9802f664fba885594333b82fe3fa3d62b6e66651
chenHX <1575197604@qq.com>
2024-08-29 20:40:29 +0800

    delete

--- .history/total_c&cpp/t193/signature_20240828204603.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @brief Invert the flag bits of an unsigned integer to a hexadecimal string. The number of bits is not complete by 0
- *
- * @param value The unsigned integer whose bits are to be inverted.
- * @return A std::string containing the hexadecimal representation of the inverted bits.
- */
-std::string convFlags(unsigned int value) {
-
-}
