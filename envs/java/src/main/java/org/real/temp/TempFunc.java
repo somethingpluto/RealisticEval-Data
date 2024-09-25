4926bf8bf0fcc9d6ff59740239a34c7e3bdd0ce5
chenHX <1575197604@qq.com>
2024-09-25 17:36:55 +0800

    fix

---
+++ envs/java/src/main/java/org/real/temp/TempFunc.java
@@ -0,0 +1,7 @@
+package org.real.temp;
+
+public class TempFunc {
+    public static void sayHello() {
+        System.out.println("hello world");
+    }
+}
--- executor/java_executor.py
+++ executor/java_executor.py
@@ -7,6 +7,9 @@ class JavaExecutor:
         self.model_name = model_name
 
     def single_run(self, code, test_code):
-        with open(f"{self._env_path}/temp.cpp", "w", encoding="utf8") as temp_file:
-            temp_file.write(code)
-            temp_file.flush()
+        with open("../envs/java/src/main/java/org/real/temp/TempFunc.java","w",encoding="utf8") as code_file:
+            code_file.write("package org.real.temp;\n")
+            code_file.write("public class TempFunc {\n")
+            code_file.write(code)
+            code_file.write()
+            code_file.flush()
